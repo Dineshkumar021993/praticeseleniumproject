cd91a88ce6f2d789c99e9cdf7d0df61d1ea3a3d2
unknown <dineshkumar.pentakota@gmail.com>
2025-12-26 14:37:15 +0530

    added options

--- conftest.py
+++ conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions, Options
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 @pytest.fixture(scope="module")
 def browser():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
     driver = webdriver.Chrome()
     url ="https://d3ldvcrr82x82a.cloudfront.net/"
     driver.get(url)
